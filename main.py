ad2c0e93a5928b753dcc9aa22c86fc1754a41755
salahs2 <75599424+salahs2@users.noreply.github.com>
2021-12-30 14:17:37 -0500

    Create Main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+class LeveredIndexing(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 1)  # Set Start Date
+        self.SetEndDate(2021, 1, 1) #Set End Date
+        self.SetCash(100000)  # Set Strategy Cash
+        
+        spy = self.AddEquity("SPY", Resolution.Daily) #adds Tick Data for SPY
+        
+        spy.SetDataNormalizationMode(DataNormalizationMode.Raw) #sets to raw data
+        
+        self.spy = spy.Symbol #assigns symbol object
+        
+        self.SetBenchmark("SPY") #Sets SP500 Benchmark
+        self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Margin)
+        
+        self.entryPrice = 0 
+        self.period = timedelta(31)
+        self.nextEntryTime = self.Time
+
+    def OnData(self, data):
+        if not self.spy in data: #checks if data is available 
+            return
+        
+        price = data[self.spy].Close #close price of day before 
+        
+        if not self.Portfolio.Invested: #checks if already invested 
+            if self.nextEntryTime <= self.Time:
+                self.SetHoldings(self.spy, 1) #sets holding to 100% of portfolio
+                self.Log("Buy Spy @" + str(price)) #logs buy order
+                self.entryPrice = price
+                
+        elif self.entryPrice * 0.9 > price: 
+            self.SetHoldings(self.spy, 1.5) #sets holdings to 150% of portfolio
+            self.Log("Buy SPY @" + str(price))
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+            Arguments:
+                data: Slice object keyed by symbol containing the stock data
+        '''
+
+        # if not self.Portfolio.Invested:
+        #    self.SetHoldings("SPY", 1)
